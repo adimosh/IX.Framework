1f518de616e552d61e9f1c756576188631c81fa0
Adrian Mos <adimosh@live.com>
2017-12-13 16:47:32 +0200

    Object pool included in StandardExtensions.

---
+++ src/IX.StandardExtensions/Efficiency/ObjectPool{T}.cs
@@ -0,0 +1,81 @@
+﻿// <copyright file="ObjectPool{T}.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+
+namespace IX.StandardExtensions.Efficiency
+{
+    /// <summary>
+    /// A pool of on-demand objects that keeps growing based on demand.
+    /// </summary>
+    /// <typeparam name="T">The class type to hold in the pool.</typeparam>
+    public class ObjectPool<T>
+        where T : class
+    {
+        // Object factory
+        private readonly Func<T> objectFactory;
+
+        // Interlocker
+        private readonly object locker;
+
+        // Pool collections
+        private Queue<T> availableObjects;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectPool{T}"/> class.
+        /// </summary>
+        /// <param name="objectFactory">An object factory for when objects need to be created.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="objectFactory"/> is <c>null</c> (<c>Nothing</c> in Visual Basic).</exception>
+        public ObjectPool(Func<T> objectFactory)
+        {
+            this.objectFactory = objectFactory ?? throw new ArgumentNullException(nameof(objectFactory));
+
+            this.locker = new object();
+            this.availableObjects = new Queue<T>();
+        }
+
+        /// <summary>
+        /// Gets the count of unused objects in the pool.
+        /// </summary>
+        /// <value>The unused object count.</value>
+        /// <remarks>
+        /// <para>If this property returns 0, there are no more unused objects in the pool. If one is requested, a new one will most likely be created.</para>
+        /// <para>You should not make decisions on new instances based on this value, as it is not synchronized with the actual pool. It is possible to get a value of 0 here and
+        /// a new item to not be created, should an object be returned to the pool between the property being accessed and the <see cref="Get"/> method called.</para>
+        /// </remarks>
+        public int Count => this.availableObjects.Count;
+
+        /// <summary>
+        /// Gets an object from the pool.
+        /// </summary>
+        /// <returns>A pooled object, either new, or unused.</returns>
+        public PooledObject<T> Get()
+        {
+            T @object;
+
+            lock (this.locker)
+            {
+                if (this.availableObjects.Count > 0)
+                {
+                    @object = this.availableObjects.Dequeue();
+                }
+                else
+                {
+                    @object = this.objectFactory();
+                }
+            }
+
+            return new PooledObject<T>(this, @object);
+        }
+
+        internal void Release(T @object)
+        {
+            lock (this.locker)
+            {
+                this.availableObjects.Enqueue(@object);
+            }
+        }
+    }
+}
---
+++ src/IX.StandardExtensions/Efficiency/PooledObject{T}.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="PooledObject{T}.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using System;
+
+namespace IX.StandardExtensions.Efficiency
+{
+    /// <summary>
+    /// A pooled object.
+    /// </summary>
+    /// <typeparam name="T">The type of class instance in the pool.</typeparam>
+    /// <seealso cref="System.IDisposable" />
+    public class PooledObject<T> : IDisposable
+        where T : class
+    {
+        private readonly T value;
+        private readonly ObjectPool<T> pool;
+
+        private bool abort;
+
+        internal PooledObject(ObjectPool<T> pool, T value)
+        {
+            this.value = value;
+            this.pool = pool;
+        }
+
+        /// <summary>
+        /// Gets the value contained.
+        /// </summary>
+        /// <value>The value.</value>
+        public T Value => this.value;
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="PooledObject{T}"/> to <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="source">The source pooled object.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static implicit operator T(PooledObject<T> source) => source.value;
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!this.abort)
+            {
+                this.pool.Release(this.value);
+            }
+        }
+
+        /// <summary>
+        /// Aborts this pooled object, not returning its value to the pool.
+        /// </summary>
+        public void Abort() => this.abort = true;
+    }
+}
