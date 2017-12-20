203a3305570d3833f2535b4b2bb8294339da25ba
adimosh <adimosh@live.com>
2017-12-20 14:41:48 +0200

    BindingLocalizer and BindingAssemblyResourceReader.

---
+++ src/IX.StandardExtensions.WPF/Localization/BindingLocalizer.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="BindingLocalizer.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace IX.StandardExtensions.WPF.Localization
+{
+    /// <summary>
+    /// A localizer converter.
+    /// </summary>
+    /// <seealso cref="System.Windows.Data.IValueConverter" />
+    public class BindingLocalizer : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(parameter is string))
+            {
+                throw new ArgumentInvalidTypeException(nameof(parameter));
+            }
+
+            if (!(value is BindingAssemblyResourceReader))
+            {
+                throw new ArgumentInvalidTypeException(nameof(parameter));
+            }
+
+            var key = parameter as string;
+            var reader = value as BindingAssemblyResourceReader;
+
+            return reader.GetLocalizedResource(key);
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        /// <exception cref="System.InvalidOperationException">This method should NEVER be called.</exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedByDesignException();
+    }
+}
