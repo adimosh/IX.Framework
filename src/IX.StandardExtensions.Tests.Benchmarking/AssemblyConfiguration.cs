7082202cca329fcbb3fd07444117b7af1c2e8ee8
adimosh <adimosh@live.com>
2018-01-08 11:35:47 +0200

    #12 - Extracted more properties and targets in external files.

---
+++ src/IX.StandardExtensions.Tests.Benchmarking/AssemblyConfiguration.cs
@@ -0,0 +1,7 @@
+// <copyright file="AssemblyConfiguration.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using Xunit;
+
+[assembly: AssemblyTrait("Category", "BenchmarkTests")]
