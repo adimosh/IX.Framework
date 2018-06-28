fc95cb48de818132ead408d13536167a366d31f7
Adrian Mos <adimosh@live.com>
2018-06-28 13:48:23 +0300

    #73 - PersistedQueue modifications.

--- src/IX.Guaranteed/Collections/PersistedQueue{T}.cs
+++ src/IX.Guaranteed/Collections/PersistedQueue{T}.cs
@@ -19,16 +19,9 @@ namespace IX.Guaranteed.Collections
     /// <typeparam name="T">The type of object in the queue.</typeparam>
     /// <seealso cref="IX.StandardExtensions.ComponentModel.DisposableBase" />
     /// <seealso cref="IX.System.Collections.Generic.IQueue{T}" />
-    public class PersistedQueue<T> : ReaderWriterSynchronizedBase, IQueue<T>
+    public class PersistedQueue<T> : PersistedQueueBase<T>
     {
-        private readonly IDirectory directoryShim;
-        private readonly IFile fileShim;
-        private readonly IPath pathShim;
-
-        private readonly Queue<PersistedQueueItem> internalQueue;
-        private readonly string dataFolderPath;
-        private readonly string poisonFolderPath;
-        private readonly DataContractSerializer serializer;
+        private readonly Queue<string> internalQueue;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PersistedQueue{T}"/> class.
@@ -49,46 +42,20 @@ namespace IX.Guaranteed.Collections
         /// </exception>
         /// <exception cref="ArgumentInvalidPathException">The folder at <paramref name="persistenceFolderPath"/> does not exist, or is not accessible.</exception>
         public PersistedQueue(string persistenceFolderPath, IFile fileShim, IDirectory directoryShim, IPath pathShim)
+            : base(persistenceFolderPath, fileShim, directoryShim, pathShim, new DataContractSerializer(typeof(T)))
         {
-            // Parameter validation
-            if (string.IsNullOrWhiteSpace(persistenceFolderPath))
-            {
-                throw new ArgumentNullException(nameof(persistenceFolderPath));
-            }
-
-            if (!(directoryShim ?? throw new ArgumentNullException(nameof(directoryShim))).Exists(persistenceFolderPath))
-            {
-                throw new ArgumentInvalidPathException(nameof(persistenceFolderPath));
-            }
-
-            // Dependent state
-            this.fileShim = fileShim ?? throw new ArgumentNullException(nameof(fileShim));
-            this.directoryShim = directoryShim;
-            this.pathShim = pathShim ?? throw new ArgumentNullException(nameof(pathShim));
-
             // Internal state
-            this.serializer = new DataContractSerializer(typeof(T));
-            this.internalQueue = new Queue<PersistedQueueItem>();
-
-            // Persistence folder paths
-            var dataFolderPath = pathShim.Combine(persistenceFolderPath, "Data");
-            this.dataFolderPath = dataFolderPath;
-            var poisonFolderPath = pathShim.Combine(persistenceFolderPath, "Poison");
-            this.poisonFolderPath = poisonFolderPath;
-
-            // Initialize folder paths
-            if (!directoryShim.Exists(dataFolderPath))
-            {
-                directoryShim.CreateDirectory(dataFolderPath);
-            }
-
-            if (!directoryShim.Exists(poisonFolderPath))
-            {
-                directoryShim.CreateDirectory(poisonFolderPath);
-            }
+            this.internalQueue = new Queue<string>();
 
             // Initialize objects
-            this.LoadExistingItems();
+#pragma warning disable HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator - Unavoidable
+            foreach (Tuple<T, string> item in this.LoadValidItemObjectHandles())
+#pragma warning restore HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator
+            {
+                this.internalQueue.Enqueue(item.Item2);
+            }
+
+            GC.Collect();
         }
 
         /// <summary>
@@ -98,45 +65,34 @@ namespace IX.Guaranteed.Collections
         /// <remarks>
         /// <para>This property is not synchronized in any way, and therefore might not reflect the true count, should there be many threads accessing it in parallel.</para>
         /// </remarks>
-        public int Count => this.InvokeIfNotDisposed((reference) => reference.ReadLock((referenceL2) => referenceL2.internalQueue.Count, reference), this);
-
-        /// <summary>
-        /// Gets an object that can be used to synchronize access to the <see cref="T:System.Collections.ICollection" />.
-        /// </summary>
-        /// <value>The synchronize root.</value>
-        object ICollection.SyncRoot => ((ICollection)this.internalQueue).SyncRoot;
-
-        /// <summary>
-        /// Gets a value indicating whether access to the <see cref="T:System.Collections.ICollection" /> is synchronized (thread safe).
-        /// </summary>
-        /// <value><c>true</c> if this instance is synchronized; otherwise, <c>false</c>.</value>
-        bool ICollection.IsSynchronized => ((ICollection)this.internalQueue).IsSynchronized;
+        public override int Count => this.InvokeIfNotDisposed((reference) => reference.ReadLock((referenceL2) => referenceL2.internalQueue.Count, reference), this);
 
         /// <summary>
         /// Clears the queue of all elements.
         /// </summary>
-        public void Clear() => this.InvokeIfNotDisposed(reference => reference.WriteLock((referenceL2) => referenceL2.internalQueue.Clear(), reference), this);
+        public override void Clear() =>
+            this.InvokeIfNotDisposed(
+                reference => reference.WriteLock(
+                    (referenceL2) =>
+                    {
+                        referenceL2.internalQueue.Clear();
+                        referenceL2.ClearData();
+                    }, reference),
+                this);
 
         /// <summary>
-        /// Verifies whether or not an item is contained in the queue.
+        /// This method should not be called, as it will always throw an <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <param name="item">The item to verify.</param>
         /// <returns><c>true</c> if the item is queued, <c>false</c> otherwise.</returns>
-        public bool Contains(T item) =>
-            this.InvokeIfNotDisposed(
-                (reference, itemL1) => reference.WriteLock(
-                    (referenceL2, itemL2) => referenceL2.internalQueue.Any(p => p.ObjectReference.Equals(itemL2)),
-                    reference,
-                    itemL1),
-                this,
-                item);
+        public override bool Contains(T item) => throw new InvalidOperationException();
 
         /// <summary>
         /// Copies the elements of the <see cref="PersistedQueue{T}" /> to an <see cref="T:System.Array" />, starting at a particular <see cref="T:System.Array" /> index.
         /// </summary>
         /// <param name="array">The one-dimensional <see cref="T:System.Array" /> that is the destination of the elements copied from <see cref="PersistedQueue{T}" />. The <see cref="T:System.Array" /> must have zero-based indexing.</param>
         /// <param name="index">The zero-based index in <paramref name="array" /> at which copying begins.</param>
-        void ICollection.CopyTo(Array array, int index) =>
+        public override void CopyTo(Array array, int index) =>
             this.InvokeIfNotDisposed(
                 (referenceL1, arrayL1, indexL1) => referenceL1.ReadLock(
                     (referenceL2, arrayL2, indexL2) => ((ICollection)referenceL2.internalQueue).CopyTo(arrayL2, indexL2),
@@ -151,10 +107,10 @@ namespace IX.Guaranteed.Collections
         /// Dequeues an item and removes it from the queue.
         /// </summary>
         /// <returns>The item that has been dequeued.</returns>
-        public T Dequeue() =>
+        public override T Dequeue() =>
             this.InvokeIfNotDisposed(
                 (referenceL1) => referenceL1.WriteLock(
-                    (referenceL2) => referenceL2.DequeuePrivate(),
+                    (referenceL2) => referenceL2.LoadTopmostItem(),
                     referenceL1),
                 this);
 
@@ -162,7 +118,7 @@ namespace IX.Guaranteed.Collections
         /// Enqueues an item, adding it to the queue.
         /// </summary>
         /// <param name="item">The item to enqueue.</param>
-        public void Enqueue(T item) =>
+        public override void Enqueue(T item) =>
             this.InvokeIfNotDisposed(
                 (referenceL1, itemL1) => referenceL1.WriteLock(
                     (referenceL2, itemL2) =>
@@ -175,118 +131,43 @@ namespace IX.Guaranteed.Collections
                 item);
 
         /// <summary>
-        /// Returns an enumerator that iterates through the collection.
+        /// This method should not be called, as it will always throw an <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <returns>An enumerator that can be used to iterate through the collection.</returns>
-        public global::System.Collections.Generic.IEnumerator<T> GetEnumerator() =>
-            this.InvokeIfNotDisposed(
-                (referenceL1) => referenceL1.ReadLock(
-                    (referenceL2) => referenceL2.internalQueue.Select(p => p.ObjectReference).ToList().GetEnumerator(),
-                    referenceL1),
-                this);
+        public override global::System.Collections.Generic.IEnumerator<T> GetEnumerator() => throw new InvalidOperationException();
 
         /// <summary>
         /// Peeks at the topmost element in the queue, without removing it.
         /// </summary>
         /// <returns>The item peeked at, if any.</returns>
-        public T Peek() =>
+        public override T Peek() =>
             this.InvokeIfNotDisposed(
                 (referenceL1) => referenceL1.ReadLock(
-                    (referenceL2) => referenceL2.internalQueue.Peek().ObjectReference,
+                    (referenceL2) => referenceL2.PeekTopmostItem(),
                     referenceL1),
                 this);
 
         /// <summary>
-        /// Copies all elements of the queue into a new array.
+        /// This method should not be called, as it will always throw an <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <returns>The created array with all element of the queue.</returns>
-        public T[] ToArray() =>
-            this.InvokeIfNotDisposed(
-                (referenceL1) => referenceL1.ReadLock(
-                    (referenceL2) => referenceL2.internalQueue.Select(p => p.ObjectReference).ToArray(),
-                    referenceL1),
-                this);
+        public override T[] ToArray() => throw new InvalidOperationException();
 
         /// <summary>
         /// Trims the excess free space from within the queue, reducing the capacity to the actual number of elements.
         /// </summary>
-        public void TrimExcess() =>
+        public override void TrimExcess() =>
             this.InvokeIfNotDisposed(
                 (referenceL1) => referenceL1.WriteLock(
                     (referenceL2) => referenceL2.internalQueue.TrimExcess(),
                     referenceL1),
                 this);
 
-#pragma warning disable HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator - Unavoidable here
-        /// <summary>
-        /// Returns an enumerator that iterates through a collection.
-        /// </summary>
-        /// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
-#pragma warning restore HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator
-
         private void EnqueuePrivate(T item)
         {
-            var i = 1;
-            string filePath = null;
+            var filePath = this.SaveNewItem(item);
 
-            DateTime now = DateTime.UtcNow;
-
-            do
-            {
-                filePath = this.pathShim.Combine(this.dataFolderPath, $"{now.ToString("yyyy.MM.dd.HH.mm.ss.fffffff")}.{i}.dat");
-                i++;
-
-                if (i == int.MaxValue)
-                {
-                    throw new InvalidOperationException();
-                }
-            }
-            while (this.fileShim.Exists(filePath));
-
-            using (global::System.IO.Stream stream = this.fileShim.Create(filePath))
-            {
-                this.serializer.WriteObject(stream, item);
-            }
-
-            this.internalQueue.Enqueue(new PersistedQueueItem(filePath, item));
-        }
-
-        private void LoadExistingItems()
-        {
-#pragma warning disable HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator - Unavoidable for now
-            foreach (var filePath in this.directoryShim.EnumerateFiles(this.dataFolderPath, "*.dat"))
-#pragma warning restore HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator
-            {
-                try
-                {
-                    using (global::System.IO.Stream stream = this.fileShim.OpenRead(filePath))
-                    {
-                        var item = (T)this.serializer.ReadObject(stream);
-                        this.internalQueue.Enqueue(new PersistedQueueItem(filePath, item));
-                    }
-                }
-                catch (SerializationException)
-                {
-                    var newFilePath = this.pathShim.Combine(this.poisonFolderPath, this.pathShim.GetFileName(filePath));
-
-                    if (this.fileShim.Exists(newFilePath))
-                    {
-                        this.fileShim.Delete(newFilePath);
-                    }
-
-                    this.fileShim.Move(filePath, newFilePath);
-                }
-            }
-        }
-
-        private T DequeuePrivate()
-        {
-            PersistedQueueItem queueItem = this.internalQueue.Dequeue();
-
-            this.fileShim.Delete(queueItem.Path);
-
-            return queueItem.ObjectReference;
+            this.internalQueue.Enqueue(filePath);
         }
 
         private readonly struct PersistedQueueItem
