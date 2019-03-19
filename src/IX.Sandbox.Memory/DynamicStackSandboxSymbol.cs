35e33c69fd5915e816e724c3c482bd3b266892fe
Adrian Mos <adimosh@outlook.com>
2019-03-19 22:18:42 +0200

    #12 - Sandbox memory R# beautification.

--- src/IX.Sandbox.Memory/DynamicStackSandboxSymbol.cs
+++ src/IX.Sandbox.Memory/DynamicStackSandboxSymbol.cs
@@ -8,19 +8,23 @@ using System.Linq;
 using System.Threading;
 using IX.Sandbox.Memory.Exceptions;
 using IX.StandardExtensions.ComponentModel;
+using JetBrains.Annotations;
 
 namespace IX.Sandbox.Memory
 {
     /// <summary>
-    /// A dynamic symbol for a list of sandbox values.
+    ///     A dynamic symbol for a list of sandbox values.
     /// </summary>
     /// <seealso cref="IX.Sandbox.Memory.SandboxSymbol" />
+    [PublicAPI]
     public class DynamicStackSandboxSymbol : SandboxSymbol
     {
-        private ConcurrentStack<SandboxValue> value;
         private SmartDisposableWeakReference<SandboxHeap> heap;
+        private ConcurrentStack<SandboxValue> value;
 
-        internal DynamicStackSandboxSymbol(SandboxHeap heap, string name)
+        internal DynamicStackSandboxSymbol(
+            SandboxHeap heap,
+            string name)
             : base(name)
         {
             this.heap = new SmartDisposableWeakReference<SandboxHeap>(heap);
@@ -28,7 +32,7 @@ namespace IX.Sandbox.Memory
         }
 
         /// <summary>
-        /// Gets or sets the raw object that is represented by this value.
+        ///     Gets or sets the raw object that is represented by this value.
         /// </summary>
         /// <value>The raw object.</value>
         public override object RawObject
@@ -38,7 +42,9 @@ namespace IX.Sandbox.Memory
                 SandboxValue item = null;
                 if (!(this.value?.TryPop(out item) ?? true))
                 {
-                    Interlocked.Exchange(ref this.value, null);
+                    Interlocked.Exchange(
+                        ref this.value,
+                        null);
                 }
 
                 return item?.RawObject;
@@ -51,99 +57,109 @@ namespace IX.Sandbox.Memory
                     throw new SandboxHeapDisposedException();
                 }
 
-                ConcurrentStack<SandboxValue> newValue = null;
-
-                if (value != null)
+                if (value == null)
                 {
-                    newValue = new ConcurrentStack<SandboxValue>();
+                    return;
+                }
 
-                    if (EnvironmentSettings.TreatSpecialArraysAsMultipleElements)
+                var newValue = new ConcurrentStack<SandboxValue>();
+
+#pragma warning disable IDISP004 // Don't ignore return value of type IDisposable. - We're not. We're placing them in a safe disposal concurrent stack
+                if (EnvironmentSettings.TreatSpecialArraysAsMultipleElements)
+                {
+                    switch (value)
                     {
-                        switch (value)
+                        case IEnumerable enumerable:
                         {
-                            case IEnumerable enumerable:
-                                {
-                                    foreach (var p in enumerable)
-                                    {
-                                        newValue.Push(h.CreateSandboxValue(p));
-                                    }
-                                }
-
-                                break;
-                            default:
-                                {
-                                    newValue.Push(h.CreateSandboxValue(value));
-                                }
-
-                                break;
+                            foreach (object p in enumerable)
+                            {
+                                newValue.Push(h.CreateSandboxValue(p));
+                            }
                         }
-                    }
-                    else
-                    {
-                        switch (value)
+
+                            break;
+                        default:
                         {
-                            case byte[] ba:
-                                {
-                                    newValue.Push(h.CreateSandboxValue(ba));
-                                }
-
-                                break;
-                            case char[] ca:
-                                {
-                                    newValue.Push(h.CreateSandboxValue(ca));
-                                }
-
-                                break;
-                            case IEnumerable enumerable:
-                                {
-                                    foreach (var p in enumerable)
-                                    {
-                                        newValue.Push(h.CreateSandboxValue(p));
-                                    }
-                                }
-
-                                break;
-                            default:
-                                {
-                                    newValue.Push(h.CreateSandboxValue(value));
-                                }
-
-                                break;
+                            newValue.Push(h.CreateSandboxValue(value));
                         }
+
+                            break;
                     }
-
-                    ConcurrentStack<SandboxValue> oldValue = Interlocked.Exchange(ref this.value, newValue);
-
-                    if (this.value != null)
+                }
+                else
+                {
+                    switch (value)
                     {
+                        case byte[] ba:
+                        {
+                            newValue.Push(h.CreateSandboxValue(ba));
+                        }
+
+                            break;
+                        case char[] ca:
+                        {
+                            newValue.Push(h.CreateSandboxValue(ca));
+                        }
+
+                            break;
+                        case IEnumerable enumerable:
+                        {
+                            foreach (object p in enumerable)
+                            {
+                                newValue.Push(h.CreateSandboxValue(p));
+                            }
+                        }
+
+                            break;
+                        default:
+                        {
+                            newValue.Push(h.CreateSandboxValue(value));
+                        }
+
+                            break;
+                    }
+                }
+#pragma warning restore IDISP004 // Don't ignore return value of type IDisposable.
+
+                ConcurrentStack<SandboxValue> oldValue = Interlocked.Exchange(
+                    ref this.value,
+                    newValue);
+
+                if (this.value != null)
+                {
 #pragma warning disable HAA0401 // Possible allocation of reference type enumerator - Yeah, nothing to do about this.
-                        foreach (SandboxValue currentValue in this.value)
+                    foreach (SandboxValue currentValue in this.value)
 #pragma warning restore HAA0401 // Possible allocation of reference type enumerator
-                        {
-                            currentValue.CaptureOne();
-                        }
-                    }
-
-                    if (oldValue != null)
                     {
-#pragma warning disable HAA0401 // Possible allocation of reference type enumerator - Yeah, nothing to do about this.
-                        foreach (SandboxValue o in oldValue)
-#pragma warning restore HAA0401 // Possible allocation of reference type enumerator
-                        {
-                            o.ReleaseOne();
-                        }
+                        currentValue.CaptureOne();
                     }
+                }
+
+                if (oldValue == null)
+                {
+                    return;
+                }
+
+#pragma warning disable HAA0401 // Possible allocation of reference type enumerator - Yeah, nothing to do about this.
+                foreach (SandboxValue o in oldValue)
+#pragma warning restore HAA0401 // Possible allocation of reference type enumerator
+                {
+                    o.ReleaseOne();
                 }
             }
         }
 
         /// <summary>
-        /// Gets or sets the string representation of this value value.
+        ///     Gets or sets the string representation of this value value.
         /// </summary>
         /// <value>The string representation.</value>
         public override string StringRepresentation
         {
-            get => string.Join(EnvironmentSettings.EnumerableSeparatorSymbol, this.value?.Select(p => p.StringRepresentation));
+            get => this.value == null
+                ? null
+                : string.Join(
+                    EnvironmentSettings.EnumerableSeparatorSymbol,
+                    this.value.Select(p => p.StringRepresentation));
 
             set
             {
@@ -160,7 +176,9 @@ namespace IX.Sandbox.Memory
                     newValue = new ConcurrentStack<SandboxValue>(newValues);
                 }
 
-                ConcurrentStack<SandboxValue> oldValue = Interlocked.Exchange(ref this.value, newValue);
+                ConcurrentStack<SandboxValue> oldValue = Interlocked.Exchange(
+                    ref this.value,
+                    newValue);
 
                 if (this.value != null)
                 {
@@ -187,12 +205,16 @@ namespace IX.Sandbox.Memory
         }
 
         /// <summary>
-        /// Disposes in the general (managed and unmanaged) context.
+        ///     Disposes in the general (managed and unmanaged) context.
         /// </summary>
         protected override void DisposeGeneralContext()
         {
-            Interlocked.Exchange(ref this.heap, null);
-            ConcurrentStack<SandboxValue> oldValue = Interlocked.Exchange(ref this.value, null);
+            Interlocked.Exchange(
+                ref this.heap,
+                null);
+            ConcurrentStack<SandboxValue> oldValue = Interlocked.Exchange(
+                ref this.value,
+                null);
 
             if (oldValue != null)
             {
--- src/IX.Sandbox.Memory/SandboxValue.cs
+++ src/IX.Sandbox.Memory/SandboxValue.cs
@@ -6,23 +6,28 @@ using System;
 using System.Threading;
 using IX.Sandbox.Memory.Exceptions;
 using IX.StandardExtensions.ComponentModel;
+using JetBrains.Annotations;
 
 namespace IX.Sandbox.Memory
 {
     /// <summary>
-    /// A value that is placed in the sandbox.
+    ///     A value that is placed in the sandbox.
     /// </summary>
     /// <remarks>
-    /// <para>All values in the sandbox are treated as heap objects.</para>
-    /// <para>When implementing a new kind of sandbox value, please keep in mind that the value lives a single entity, and cannot be split (say, when simulating an array).</para>
+    ///     <para>All values in the sandbox are treated as heap objects.</para>
+    ///     <para>
+    ///         When implementing a new kind of sandbox value, please keep in mind that the value lives a single entity, and
+    ///         cannot be split (say, when simulating an array).
+    ///     </para>
     /// </remarks>
     /// <seealso cref="IX.StandardExtensions.ComponentModel.DisposableBase" />
+    [PublicAPI]
     public abstract class SandboxValue : DisposableBase
     {
         private int timesCaptured;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SandboxValue"/> class.
+        ///     Initializes a new instance of the <see cref="SandboxValue" /> class.
         /// </summary>
         protected SandboxValue()
         {
@@ -30,51 +35,54 @@ namespace IX.Sandbox.Memory
         }
 
         /// <summary>
-        /// Gets the identifier.
+        ///     Gets the identifier.
         /// </summary>
         /// <value>The identifier.</value>
         public Guid Id { get; }
 
+        internal int TimesCaptured => this.timesCaptured;
+
+        internal bool CertainForCollection { get; set; }
+
+        internal bool MarkedForCollection { get; set; }
+
         /// <summary>
-        /// Gets or sets the raw object that is represented by this value.
+        ///     Gets or sets the raw object that is represented by this value.
         /// </summary>
         /// <value>The raw object.</value>
         public abstract object RawObject { get; protected set; }
 
         /// <summary>
-        /// Gets or sets the string representation of this value value.
+        ///     Gets or sets the string representation of this value value.
         /// </summary>
         /// <value>The string representation.</value>
         public abstract string StringRepresentation { get; protected set; }
 
-        internal int TimesCaptured => this.timesCaptured;
-
-        internal bool MarkedForCollection { get; set; }
-
-        internal bool CertainForCollection { get; set; }
-
         /// <summary>
-        /// Returns a <see cref="string" /> that represents this instance.
+        ///     Returns a <see cref="string" /> that represents this instance.
         /// </summary>
         /// <returns>A <see cref="string" /> that represents this instance.</returns>
         public override string ToString() => this.StringRepresentation;
 
         /// <summary>
-        /// Returns a hash code for this instance.
+        ///     Returns a hash code for this instance.
         /// </summary>
         /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
         public override int GetHashCode() => this.Id.GetHashCode();
 
         /// <summary>
-        /// Determines whether the specified <see cref="object" /> is equal to this instance.
+        ///     Determines whether the specified <see cref="object" /> is equal to this instance.
         /// </summary>
         /// <param name="obj">The object to compare with the current object.</param>
-        /// <returns><see langword="true"/> if the specified <see cref="object" /> is equal to this instance; otherwise, <see langword="false"/>.</returns>
+        /// <returns>
+        ///     <see langword="true" /> if the specified <see cref="object" /> is equal to this instance; otherwise,
+        ///     <see langword="false" />.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (obj is SandboxValue sv)
             {
-                return sv?.Id == this.Id;
+                return sv.Id == this.Id;
             }
 
             return false;
