50cde309e677c14af4d5fd53aee15176a45a5621
Adrian Mos <adimosh@outlook.com>
2019-03-19 20:48:35 +0200

    #12 - Some warnings removed from IX.Math unit tests.

--- src/IX.UnitTests/IX.Math/FunctionsListingUnitTest.cs
+++ src/IX.UnitTests/IX.Math/FunctionsListingUnitTest.cs
@@ -8,19 +8,19 @@ using Xunit;
 namespace IX.UnitTests.IX.Math
 {
     /// <summary>
-    /// Tests for function listings.
+    ///     Tests for function listings.
     /// </summary>
     public class FunctionsListingUnitTest
     {
         /// <summary>
-        /// Test functionality that gets the available functions test.
+        ///     Test functionality that gets the available functions test.
         /// </summary>
         [Fact(DisplayName = "Test getting available functions and parameters.")]
         public void GetAvailableFunctionsTest()
         {
             using (var service = new ExpressionParsingService())
             {
-                var functions = service.GetRegisteredFunctions();
+                string[] functions = service.GetRegisteredFunctions();
 
                 Assert.True(functions.Length > 0);
             }
