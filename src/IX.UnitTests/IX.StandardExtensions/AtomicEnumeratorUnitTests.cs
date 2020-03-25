7a295a397098bc7c9d57140532c568e971411586
Adrian Mos <adrian.mos.bp@nttdata.ro>
2020-03-25 08:52:11 +0200

    Two extra tests for AtomicEnumerator.

--- src/IX.UnitTests/IX.StandardExtensions/AtomicEnumeratorUnitTests.cs
+++ src/IX.UnitTests/IX.StandardExtensions/AtomicEnumeratorUnitTests.cs
@@ -17,36 +17,133 @@ namespace IX.UnitTests.IX.StandardExtensions
         /// <summary>
         /// Unit test for the <see cref="AtomicEnumerator{TItem, TEnumerator}"/> correctness of enumeration.
         /// </summary>
-        [Fact]
-        public void AtomicEnumeratorCorrectEnumerationTest()
+        [Fact(DisplayName = "AtomicEnumerator direct instantiation test")]
+        public void Test1()
         {
             // ARRANGE
-            var q = new List<int> { 1, 2, 3, 4, 5 };
-
-            using (List<int>.Enumerator enumerator = q.GetEnumerator())
+            var q = new List<int>
             {
-                List<int> newList1 = new List<int>(5), newList2 = new List<int>(5);
+                1,
+                2,
+                3,
+                4,
+                5
+            };
 
-                using (var ae = new AtomicEnumerator<int, List<int>.Enumerator>(enumerator, () => new ReadOnlySynchronizationLocker(null)))
-                {
-                    // ACT
-                    while (ae.MoveNext())
-                    {
-                        newList1.Add(ae.Current);
-                    }
+            using List<int>.Enumerator enumerator = q.GetEnumerator();
 
-                    ae.Reset();
+            List<int> newList1 = new List<int>(5), newList2 = new List<int>(5);
 
-                    while (ae.MoveNext())
-                    {
-                        newList2.Add(ae.Current);
-                    }
-                }
+            using var ae = new AtomicEnumerator<int, List<int>.Enumerator>(
+                enumerator,
+                () => new ReadOnlySynchronizationLocker(null));
 
-                // ASSERT
-                Assert.True(q.SequenceEquals(newList1));
-                Assert.True(q.SequenceEquals(newList2));
+            // ACT
+            while (ae.MoveNext())
+            {
+                newList1.Add(ae.Current);
             }
+
+            ae.Reset();
+
+            while (ae.MoveNext())
+            {
+                newList2.Add(ae.Current);
+            }
+
+            // ASSERT
+            Assert.True(q.SequenceEquals(newList1));
+            Assert.True(q.SequenceEquals(newList2));
+        }
+
+        /// <summary>
+        /// Unit test for the <see cref="AtomicEnumerator{TItem, TEnumerator}"/> correctness of enumeration.
+        /// </summary>
+        [Fact(DisplayName = "AtomicEnumerator FromCollection test")]
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Performance",
+            "HAA0401:Possible allocation of reference type enumerator",
+            Justification = "We're dealing with an AtomicEnumerator.")]
+        public void Test2()
+        {
+            // ARRANGE
+            var q = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            List<int> newList1 = new List<int>(5), newList2 = new List<int>(5);
+
+            // ACT
+            using var ae = AtomicEnumerator<int>.FromCollection(
+                q,
+                () => new ReadOnlySynchronizationLocker(null));
+
+            while (ae.MoveNext())
+            {
+                newList1.Add(ae.Current);
+            }
+
+            ae.Reset();
+
+            while (ae.MoveNext())
+            {
+                newList2.Add(ae.Current);
+            }
+
+            // ASSERT
+            Assert.True(q.SequenceEquals(newList1));
+            Assert.True(q.SequenceEquals(newList2));
+        }
+
+        /// <summary>
+        /// Unit test for the <see cref="AtomicEnumerator{TItem, TEnumerator}"/> correctness of enumeration.
+        /// </summary>
+        [Fact(DisplayName = "AtomicEnumerator FromEnumerator test")]
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Performance",
+            "HAA0401:Possible allocation of reference type enumerator",
+            Justification = "We're dealing with an AtomicEnumerator.")]
+        public void Test3()
+        {
+            // ARRANGE
+            var q = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            using List<int>.Enumerator enumerator = q.GetEnumerator();
+
+            List<int> newList1 = new List<int>(5), newList2 = new List<int>(5);
+
+            // ACT
+            using var ae = AtomicEnumerator<int>.FromEnumerator(
+                enumerator,
+                () => new ReadOnlySynchronizationLocker(null));
+
+            while (ae.MoveNext())
+            {
+                newList1.Add(ae.Current);
+            }
+
+            ae.Reset();
+
+            while (ae.MoveNext())
+            {
+                newList2.Add(ae.Current);
+            }
+
+            // ASSERT
+            Assert.True(q.SequenceEquals(newList1));
+            Assert.True(q.SequenceEquals(newList2));
         }
     }
 }
